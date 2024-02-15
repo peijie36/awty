d282ee9593c85ab4fa4af9ce25e4e3068f08533c
peijie36 <peijiezheng2001@gmail.com>
2024-02-15 01:43:59 -0800

    Finish hw

--- app/src/main/java/edu/uw/ischool/peijie36/awty/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/peijie36/awty/MainActivity.kt
@@ -1,59 +1,74 @@
 package edu.uw.ischool.peijie36.awty
 
+import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var userMessage : EditText
-    private lateinit var phoneNum : EditText
-    private lateinit var duration : EditText
+    private lateinit var userMessageET : EditText
+    private lateinit var phoneNumET : EditText
+    private lateinit var durationET : EditText
     private lateinit var controlButton : Button
-    private var isSending = false
-    private lateinit var handler: Handler
-    private lateinit var runnable: Runnable
+    private var isServiceRunning = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        userMessage = findViewById(R.id.et_message)
-        phoneNum = findViewById(R.id.et_phone)
-        duration = findViewById(R.id.et_minutes)
+        userMessageET = findViewById(R.id.et_message)
+        phoneNumET = findViewById(R.id.et_phone)
+        durationET = findViewById(R.id.et_minutes)
         controlButton = findViewById(R.id.btn_control)
 
-        handler = Handler(Looper.getMainLooper())
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
-        val delayInMins = (duration.text.toString().toIntOrNull() ?: 0) * 1000
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                // Check if all fields are entered to enable the button
+                val messageNotEmpty = userMessageET.text.isNotEmpty()
+                val phoneNumberNotEmpty = phoneNumET.text.isNotEmpty()
+                val delayNotEmpty = durationET.text.isNotEmpty()
+                controlButton.isEnabled = messageNotEmpty && phoneNumberNotEmpty && delayNotEmpty
+            }
+        }
+
+        userMessageET.addTextChangedListener(textWatcher)
+        phoneNumET.addTextChangedListener(textWatcher)
+        durationET.addTextChangedListener(textWatcher)
 
         controlButton.setOnClickListener {
-            isSending = !isSending
-            if (isSending) {
-                sendMessages(delayInMins)
+            if (!isServiceRunning) {
+                runService()
             } else {
-                stopMessages()
+                stopService()
             }
         }
     }
 
-    private fun sendMessages(delay : Int) {
-        handler.postDelayed({
-            val message = userMessage.text.toString()
-            val phone = phoneNum.text.toString()
-            Toast.makeText(this,"${phone}: ${message}", Toast.LENGTH_LONG).show()
-        }, delay.toLong())
+    private fun runService() {
+        val message = userMessageET.text.toString()
+        val phoneNumber = phoneNumET.text.toString()
+        val delayInMins = (durationET.text.toString().toIntOrNull() ?: 0) * 1000 * 60
 
+        val intent = Intent(this, MessageService::class.java)
+        intent.putExtra("message", message)
+        intent.putExtra("phoneNumber", phoneNumber)
+        intent.putExtra("delay", delayInMins)
+        startService(intent)
+        isServiceRunning = true
         controlButton.text = "Stop"
     }
 
-    private fun stopMessages() {
-        // Remove any pending messages from the handler
-        handler.removeCallbacksAndMessages(null)
-
+    private fun stopService() {
+        val intent = Intent(this, MessageService::class.java)
+        stopService(intent)
+        isServiceRunning = false
         controlButton.text = "Start"
     }
 }
